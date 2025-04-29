977e02fda7737aaed5afa2f2d39b4289c4da9acb
MiniLaken <tanielul3741@masseyhigh.school.nz>
2025-04-29 14:54:28 +1200

    Update B_01_rps_game_v1.py

--- B_01_rps_game_v1.py
+++ B_01_rps_game_v1.py
@@ -1,3 +1,5 @@
+import random
+
 # checks for an integer more than 0 (allows <enter>)
 def int_checker(question):
     while True:
@@ -20,6 +22,10 @@ def int_checker(question):
 
         except ValueError:
             print(error)
+
+#compares user / computer choice and returns
+# results ( win / lose / tie)
+
 
 
 # Main routine start here
@@ -60,18 +66,24 @@ while rounds_player <= num_rounds:
         rounds_heading = f"\n💿💿💿 Round {rounds_player + 1} of {num_rounds}💿💿💿"
 
         print(rounds_heading)
-        print()   
+
+        # randomly chosose from the rps list(excluding the exit code)
+    comp_choice = random. choice(rps_list[:-1])
+    print(" Computer choice", comp_choice)
 
 
     # get user choice
     user_choice = string_checker("Choose: ", rps_list)
     print("you chose", user_choice)
    
-# if user choice is the exit code, break the loop
-if user_choice == "xxx":
-    break
+    # if user choice is the exit code, break the loop
+    if user_choice == "xxx":
+        break
         
-rounds_player += 1
+    result = rps_compare(user_choice, comp_choice)
+    print(f"{user_choice} vs {comp_choice}{result}")
+
+    rounds_player += 1
     
 
 # if users are in infinite mode, increase number of rounds!
@@ -84,136 +96,4 @@ if mode == "infinite":
 
 # Game History / Statistics area
 
-        # checks for an integer more than 0 (allows <enter>)
-def int_checker(question):
-    while True:
-        error = " Please enter an integer that is 1 or more."
-
-        to_check = input(question)
-
-        # check for infinte mode
-        if to_check == "":
-            return "infinite"
-
-        try:
-            response = int(to_check)
-
-            # checks that the number is more than / equal to 1
-            if response < 1:
-                print(error)
-            else:
-                return response
-
-        except ValueError:
-            print(error)
-
-
-# Main routine start here
-
-# Intialise game variables
-mode = "regular"
-rounds_player = 0
-
-
-print("💎📰✂ Rock / Paper / Scissors Game 💎📰✂")
-print()
-
-# Instruction 
-
-# Ask user for number of rounds / infinite mode
-num_rounds = int_checker("How many rounds would you like? Push <enter> for infinite mode: ")
-
-if num_rounds == "infinite":
-    mode = "infinite"
-    num_rounds = 5
-
-
-# Game loop starts here
-while rounds_player <= num_rounds:
-    if mode == "infinite":
-        rounds_heading = "\n Round {rounds_played + 1} (Infinite Mode) "
-    else:
-        rounds_heading = f"\n💿💿💿 Round {rounds_player + 1} of {num_rounds}💿💿💿"
-
-        print(rounds_heading)
-        print()    
-    
-    user_choice = input("Choose: ")
-    if user_choice == "xxx":
-        break
         
-    rounds_player += 1
-    
-
-        # if users are in infinite mode, increase number of rounds!
-    if mode == "infinite":
-        num_rounds += 1
-
-
-
-# Game loop ends here
-
-# Game History / Statistics area# checks for an integer more than 0 (allows <enter>)
-def int_checker(question):
-    while True:
-        error = " Please enter an integer that is 1 or more."
-
-        to_check = input(question)
-
-        # check for infinte mode
-        if to_check == "":
-            return "infinite"
-
-        try:
-            response = int(to_check)
-
-            # checks that the number is more than / equal to 1
-            if response < 1:
-                print(error)
-            else:
-                return response
-
-        except ValueError:
-            print(error)
-
-
-# Main routine start here
-
-# Intialise game variables
-mode = "regular"
-rounds_player = 0
-
-
-print("💎📰✂ Rock / Paper / Scissors Game 💎📰✂")
-print()
-
-# Instruction 
-
-# Ask user for number of rounds / infinite mode
-num_rounds = int_checker("How many rounds would you like? Push <enter> for infinite mode: ")
-
-if num_rounds == "infinite":
-    mode = "infinite"
-    
-    num_rounds = 5
-
-
-# Game loop starts here
-while rounds_player <= num_rounds:
-    
-    user_choice = input("Choose: ")
-    if user_choice == "xxx":
-        break
-        
-    rounds_player += 1
-    print("rounds played: ", rounds_player)
-
-        # if users are in infinite mode, increase number of rounds!
-    if mode == "infinite":
-        num_rounds += 1
-
-print("We are done")
-
-# Game loop ends here
-
-# Game History / Statistics area
